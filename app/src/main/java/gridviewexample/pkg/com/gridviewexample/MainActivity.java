41cf006bc1292e2e4aa1c236550d9d1acbec67cc
Rama naidu <parimi.ramanaidu@gmail.com>
2016-03-02 23:56:15 +0530

    add rama2

--- app/src/main/java/gridviewexample/pkg/com/gridviewexample/MainActivity.java
+++ app/src/main/java/gridviewexample/pkg/com/gridviewexample/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-
+//rama
 
 		 GridView gridview = (GridView) findViewById(R.id.gridview);
 		    gridview.setAdapter(new ImageAdapter(this));
